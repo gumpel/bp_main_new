4db0caae640a85ccceeac178d536cea72a543aaf
Gumpel <grzesiek.gumplowicz@gmail.com>
2017-01-13 13:13:23 +0100

    isoscope

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -85,131 +85,7 @@ jQuery(document).ready(function(t) {
 });
 
 // fog generator 
-$(document).ready(function() {
-    if ($(window).width() >= 992 && $('#fog').length != -1) {
-        var canvasWidth = 1600,
-            canvasHeight = 200,
-            pCount = 0,
-            pCollection = new Array();
 
-        var puffs = 1;
-        var particlesPerPuff = 2000;
-        var img = '../images/smoke2.png';
-
-        var smokeImage = new Image();
-        smokeImage.src = img;
-
-        for (var i1 = 0; i1 < puffs; i1++) {
-            var puffDelay = i1 * 1500; //300 ms between puffs
-
-            for (var i2 = 0; i2 < particlesPerPuff; i2++) {
-                addNewParticle((i2 * 50) + puffDelay);
-            }
-        }
-
-
-        draw(new Date().getTime(), 3000)
-
-
-
-        function addNewParticle(delay) {
-
-            var p = {};
-            p.top = canvasHeight;
-            p.left = randBetween(-200, 800);
-
-            p.start = new Date().getTime() + delay;
-            p.life = 8000;
-            p.speedUp = 30;
-
-
-            p.speedRight = randBetween(0, 20);
-
-            p.rot = randBetween(-1, 1);
-            p.red = Math.floor(randBetween(0, 255));
-            p.blue = Math.floor(randBetween(0, 255));
-            p.green = Math.floor(randBetween(0, 255));
-
-
-            p.startOpacity = .3
-            p.newTop = p.top;
-            p.newLeft = p.left;
-            p.size = 200;
-            p.growth = 10;
-
-            pCollection[pCount] = p;
-            pCount++;
-
-
-        }
-
-        function draw(startT, totalT) {
-            //Timing
-            var timeDelta = new Date().getTime() - startT;
-            var stillAlive = false;
-
-            //Grab and clear the canvas
-            var c = document.getElementById('fog');
-            var ctx = c.getContext('2d');
-            ctx.clearRect(0, 0, c.width, c.height);
-            c.width = c.width;
-
-            //Loop through particles
-            for (var i = 0; i < pCount; i++) {
-                //Grab the particle
-                var p = pCollection[i];
-
-                //Timing
-                var td = new Date().getTime() - p.start;
-                var frac = td / p.life
-
-                if (td > 0) {
-                    if (td <= p.life) { stillAlive = true; }
-
-                    //attributes that change over time
-                    var newTop = p.top - (p.speedUp * (td / 1000));
-                    var newLeft = p.left + (p.speedRight * (td / 1000));
-                    var newOpacity = Math.max(p.startOpacity * (1 - frac), 0);
-
-                    var newSize = p.size + (p.growth * (td / 1000));
-                    p.newTop = newTop;
-                    p.newLeft = newLeft;
-
-                    //Draw!
-                    ctx.fillStyle = 'rgba(150,150,150,' + newOpacity + ')';
-                    ctx.globalAlpha = newOpacity;
-                    ctx.drawImage(smokeImage, newLeft, newTop, newSize, newSize);
-                }
-            }
-
-
-
-            //Repeat if there's still a living particle
-            if (stillAlive) {
-                requestAnimationFrame(function() { draw(startT, totalT); });
-            } else {
-                clog(timeDelta + ': stopped');
-            }
-        }
-
-        function randBetween(n1, n2) {
-            var r = (Math.random() * (n2 - n1)) + n1;
-            return r;
-        }
-
-        function randOffset(n, variance) {
-            //e.g. variance could be 0.1 to go between 0.9 and 1.1
-            var max = 1 + variance;
-            var min = 1 - variance;
-            var r = Math.random() * (max - min) + min;
-            return n * r;
-        }
-
-        function clog(s) {
-            console.log(s);
-        }
-    }
-})
 
 
 $(document).ready(function() {
@@ -1292,36 +1168,23 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     if ($('#portfoliogrid').length) {
-        var $grid = $('#portfoliogrid')
-        $grid.isotope({
+        //alert('obrazki wgrane 4');
+        var $gridp = $('#portfoliogrid');
+        $gridp.imagesLoaded(function() {
+        $gridp.isotope({
             // options
             itemSelector: '.grid-item',
             masonry: {
                 columnWidth: '.grid-sizer',
                 percentPosition: true,
             }
-        }).imagesLoaded(function() {
-            // trigger again after images have loaded
-            $grid.isotope('reLayout');
-        });
-        $(window).load(function() {
-            // trigger again after everything has loaded
-            $grid.isotope('reLayout');
+        })
         });
 
-
-
-
-
-        $('.pull-down').each(function() {
-            var $this = $(this);
-            $this.css('margin-top', $this.parent().height() - $this.height())
-        });
 
         // $grid.isotope({ filter: '.portale' });
-        $('.portfolioselect').on('changed.bs.select', function() {
-            //console.log('.' + $('.portfolioselect').selectpicker('val'))
-            $grid.isotope({ filter: '.' + $('.portfolioselect').selectpicker('val') });
+        $('.portfolioselect').on('changed.bs.select', function() {            
+            $gridp.isotope({ filter: '.' + $('.portfolioselect').selectpicker('val') });
         });
     }
     if ($('.servises-detail').length) {
@@ -1334,11 +1197,11 @@ $(document).ready(function() {
             }
         }).imagesLoaded(function() {
             // trigger again after images have loaded
-            $grid.isotope('reLayout');
+            $grid.isotope('reloadItems');
         });
-        $(window).load(function() {
+        $(window).on('load',function() {
             // trigger again after everything has loaded
-            $grid.isotope('reLayout');
+            $grid.isotope('reloadItems');
         });
 
         // $grid.isotope({ filter: '.portale' });
